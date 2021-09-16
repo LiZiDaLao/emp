385ec615471076793c19296480adad755ed7b956
LiZiDaLao <90842111+LiZiDaLao@users.noreply.github.com>
2021-09-16 20:52:52 +0800

    Update UserDao.java

--- src/main/java/com/example/dao/UserDao.java
+++ src/main/java/com/example/dao/UserDao.java
@@ -2,7 +2,7 @@ package com.example.dao;
 
 import com.example.entity.User;
 import org.springframework.stereotype.Component;
-
+@Compont
 public interface UserDao {
     //查
     User selectUser(String username);
